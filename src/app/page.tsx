ce71901bee4aa26d22ba805cb484425da7e8a2c0
davideCoderblock <davide@coderblock.com>
2023-09-22 18:06:54 +0200

    (fix img and change metric on imperial temperature)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import React, { useState, ChangeEvent } from "react";
+import { WeatherData } from "./shared/weatherData";
+import Image from "next/image";
 
 export default function Home() {
   const [city, setCity] = useState<string>("");
-  const [weatherData, setWeatherData] = useState<any>();
+  const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
 
   const APIKey: string = "e142aa80633f6b8932694e428510798d";
 
@@ -16,7 +18,7 @@ export default function Home() {
     console.log(`Meteo per la città: ${city}`);
     try {
       const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&units=standard`
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&units=metric`
       );
 
       const data = await response.json();
@@ -58,14 +60,17 @@ export default function Home() {
         >
           Inserisci
         </button>
-        {weatherData && Object.keys(weatherData).length !== 0 ? (
+        {weatherData ? (
           <div className="mt-4">
-            <img
+            <Image
               src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
               alt="Weather Icon"
               className="w-16 h-16 mx-auto"
+              width={100}
+              height={100}
             />
             <p className="text-4xl font-semibold text-black">
+              {weatherData.name}
               Currently {Math.round(weatherData.main.temp)}°C
             </p>
           </div>
